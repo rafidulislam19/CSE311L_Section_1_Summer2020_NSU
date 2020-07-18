d851de27129d355106135a740224f92684a1f7af
rafidulislam19 <rafidul.islam19@northsouth.edu>
2020-07-18 01:51:36 -0700

    Lab Assessment

---
+++ Lab Asssessment Submission/Lab Assessment 2/Connection.php
@@ -0,0 +1,15 @@
+<?php
+
+$MySQLi_connection = new MySQLi('localhost','root','','homework1');
+
+
+if ($MySQLi_connection->connect_error)
+{
+    echo "Not Connected, error:" .$MySQLi_connection->connect_error;
+}
+else
+{
+ echo "Connected";
+}
+
+?>
